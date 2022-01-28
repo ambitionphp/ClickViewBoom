fd125a8ae70fead65c430c96219d12c55dca5f55
Steven Sarkisian <ambitionphp@gmail.com>
2022-01-28 16:09:02 -0500

    new logo

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,5 +1,42 @@
 <a href="/">
-    <span style="font-size: 4rem;line-height: 4.5rem;">
-        💣
-    </span>
+    <svg style="width: 85px;margin: 0 auto .3rem;" id="Icons" viewBox="0 0 48 48" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <defs id="defs908">
+            <style id="style906">
+                .cls-2 {
+                    fill: #656769
+                }
+
+                .cls-3 {
+                    fill: #87898c
+                }
+
+                .cls-4,
+                .cls-5 {
+                    fill: none;
+                    stroke: #45413c;
+                    stroke-linecap: round;
+                    stroke-linejoin: round
+                }
+
+                .cls-5 {
+                    fill: #ff6242
+                }
+            </style>
+        </defs>
+        <g id="_Group_" data-name="&lt;Group&gt;">
+            <ellipse id="_Ellipse_" data-name="&lt;Ellipse&gt;" cx="19.54" cy="45.5" rx="9.62" ry="1.5" style="fill:#45413c;opacity:.15" />
+            <path id="_Path_" data-name="&lt;Path&gt;" class="cls-2" style="fill:#6b7280;fill-opacity:1" d="m21.58 26.95 2.54-4.31 4.64 5.05-3.86 2.22-3.32-2.96z" />
+            <path id="_Path_2" data-name="&lt;Path&gt;" class="cls-3" style="fill:#9ca3af;fill-opacity:1" d="m23.22 28.39 2.52-3.99-1.62-1.76-2.54 4.31 1.64 1.44z" />
+            <path id="_Path_3" data-name="&lt;Path&gt;" class="cls-4" style="stroke:#1f2937;stroke-opacity:1" d="m21.58 26.95 2.54-4.31 4.64 5.05-3.86 2.22-3.32-2.96z" />
+            <circle id="_Path_4" data-name="&lt;Path&gt;" class="cls-2" cx="17.29" cy="35.04" r="10.46" transform="rotate(-35.14 17.292 35.046)" style="fill:#6b7280;fill-opacity:1" />
+            <path id="_Path_5" data-name="&lt;Path&gt;" class="cls-3" d="M18.44 28.44a10.47 10.47 0 0 1 8.66 10.21 10.85 10.85 0 0 0 .5-1.82A10.46 10.46 0 1 0 7 33.25a10 10 0 0 0-.15 1.88 10.46 10.46 0 0 1 11.59-6.69Z" style="fill:#9ca3af;fill-opacity:1" />
+            <circle id="_Path_6" data-name="&lt;Path&gt;" class="cls-4" cx="17.29" cy="35.04" r="10.46" transform="rotate(-35.14 17.292 35.046)" style="stroke:#1f2937;stroke-opacity:1" />
+            <path id="_Path_7" data-name="&lt;Path&gt;" class="cls-4" d="M26.55 25.29a8.28 8.28 0 0 0 2.61-6.12" style="stroke:#1f2937;stroke-opacity:1" />
+            <path id="_Path_8" data-name="&lt;Path&gt;" class="cls-5" d="M28.85 8.56 32 4.73a.42.42 0 0 1 .73.16l1.58 6.28a.41.41 0 0 0 .33.31l6.11 1.07a.42.42 0 0 1 .13.78l-6.37 3.38a.42.42 0 0 0-.2.49l1.56 5.21a.42.42 0 0 1-.62.48l-5.93-3.66a.45.45 0 0 0-.39 0L24.8 21a.42.42 0 0 1-.59-.45l.61-3.47a.42.42 0 0 0-.23-.45l-6-3a.42.42 0 0 1 .07-.78L24 11.33a.43.43 0 0 0 .3-.39l.13-4.34a.41.41 0 0 1 .66-.32l3.2 2.35a.42.42 0 0 0 .56-.07Z" style="fill:#ef4444;fill-opacity:1;stroke:#1f2937;stroke-opacity:1" />
+            <path id="_Path_9" data-name="&lt;Path&gt;" class="cls-5" d="m37.41 6.13.59-3.4a.51.51 0 0 1 .85-.3l1.64 1.45a.51.51 0 0 1 0 .78l-2.24 2a.51.51 0 0 1-.84-.53Z" style="stroke:#1f2937;stroke-opacity:1;fill:#ef4444;fill-opacity:1" />
+            <path id="_Path_10" data-name="&lt;Path&gt;" class="cls-5" d="M19.31 8.29 15.83 6a.51.51 0 0 0-.78.52l.51 2.65a.51.51 0 0 0 .56.41l3-.34a.5.5 0 0 0 .19-.95Z" style="stroke:#1f2937;stroke-opacity:1;fill:#ef4444;fill-opacity:1" />
+            <path id="_Path_11" data-name="&lt;Path&gt;" class="cls-5" d="m40.7 19.72 3.45.6a.42.42 0 0 1 .32.56l-.82 2.2a.42.42 0 0 1-.7.14l-2.63-2.8a.42.42 0 0 1 .38-.7Z" style="stroke:#1f2937;stroke-opacity:1;fill:#ef4444;fill-opacity:1" />
+            <path id="_Path_12" data-name="&lt;Path&gt;" d="m29.64 11.82.85-.86a.42.42 0 0 1 .71.25l.28 2.19a.4.4 0 0 0 .33.36l1.8.36-2.33 1.17a.37.37 0 0 0-.22.29l-.23 1.12a.42.42 0 0 1-.71.22l-1.48-1.49a.57.57 0 0 0-.13-.08l-1.92-.83a.42.42 0 0 1 0-.77l.9-.36a.43.43 0 0 0 .26-.39v-1.13a.41.41 0 0 1 .6-.37l.8.4a.44.44 0 0 0 .49-.08Z" style="fill:#eab308;fill-opacity:1;stroke:#1f2937;stroke-opacity:1;stroke-linecap:round;stroke-linejoin:round" />
+        </g>
+    </svg>
 </a>
