2e8518eb5329d9fe4f93cc84077cd5e6b3d5d6ad
Steven Sarkisian <ambitionphp@gmail.com>
2022-01-28 16:20:17 -0500

    favicons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,27 +4,17 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         {!! SEO::generate() !!}
-
-        <!-- Fonts -->
+        @include('layouts.favicon')
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
         @livewireStyles
-
-        <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
-
         <div class="min-h-screen bg-gray-100">
             @livewire('navigation-menu')
-
-            <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
                     <div class="max-w-xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
@@ -32,17 +22,12 @@
                     </div>
                 </header>
             @endif
-
-            <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
-
             @include('footer-menu')
         </div>
-
         @stack('modals')
-
         @livewireScripts
     </body>
 </html>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,16 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
+        @include('layouts.favicon')
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
-        <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body>
