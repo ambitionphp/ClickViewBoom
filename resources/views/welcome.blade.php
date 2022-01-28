b278166a050855e48dffd0e2da9125a2e0539ae7
Steven Sarkisian <ambitionphp@gmail.com>
2022-01-28 12:34:01 -0500

    font size adjustments

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,8 @@
     <div class="py-2">
         <div class="max-w-xl mx-auto px-6 sm:px-6 lg:px-8">
             <div class="pb-5">
-                <h3 class="text-center font-semibold text-l leading-tight">Paste a password, secret message or private link below.</h3>
-                <h5 class="text-center text-gray-500">Keep sensitive info out of your email and chat logs.</h5>
+                <h3 class="text-center font-semibold text-xl leading-tight">Paste a password, secret message or private link below.</h3>
+                <h5 class="text-center text-xl text-gray-500">Keep sensitive info out of your email and chat logs.</h5>
             </div>
 
             <livewire:create-text-form />
