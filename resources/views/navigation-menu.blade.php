ed97b92fdcebdf04133751c2becc47ee51b78f1e
Steven Sarkisian <ambitionphp@gmail.com>
2022-01-31 23:23:48 -0500

    Emergency flush

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,33 +1,34 @@
-<nav x-data="{ open: false }" class="bg-gray-100">
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
-        <div class="flex justify-between h-8">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ url('') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
-                    </a>
+<div>
+    <nav x-data="{ open: false }" class="bg-gray-100">
+        <!-- Primary Navigation Menu -->
+        <div class="max-w-xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+            <div class="flex justify-between h-8">
+                <div class="flex">
+                    <!-- Logo -->
+                    <div class="shrink-0 flex items-center">
+                        <a href="{{ url('') }}">
+                            <x-jet-application-mark class="block h-9 w-auto" />
+                        </a>
+                    </div>
+                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                        <x-jet-nav-link href="{{ route('about') }}" :active="request()->routeIs('about')">
+                            {{ __('About') }}
+                        </x-jet-nav-link>
+                    </div>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="{{ route('about') }}" :active="request()->routeIs('about')">
-                        {{ __('About') }}
-                    </x-jet-nav-link>
-                </div>
-            </div>
 
-            <div class="flex">
+                <div class="flex">
                 @auth
-                <!-- Settings Dropdown -->
-                <div class="ml-3 relative">
-                    <x-jet-dropdown align="right" width="48">
-                        <x-slot name="trigger">
-                            @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                                <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                    <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                                </button>
-                            @else
-                                <span class="inline-flex rounded-md">
+                    <!-- Settings Dropdown -->
+                        <div class="ml-3 relative">
+                            <x-jet-dropdown align="right" width="48">
+                                <x-slot name="trigger">
+                                    @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                                        <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                            <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                        </button>
+                                    @else
+                                        <span class="inline-flex rounded-md">
                                     <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-600 focus:outline-none transition">
                                         <i class="fas fa-user-secret"></i>
 
@@ -36,58 +37,59 @@
                                         </svg>
                                     </button>
                                 </span>
-                            @endif
-                        </x-slot>
+                                    @endif
+                                </x-slot>
 
-                        <x-slot name="content">
-                            <!-- Account Management -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
-                            </div>
+                                <x-slot name="content">
+                                    <!-- Account Management -->
+                                    <div class="block px-4 py-2 text-xs text-gray-400">
+                                        {{ __('Manage Account') }}
+                                    </div>
 
-                            <x-jet-dropdown-link href="{{ route('recent') }}">
-                                {{ __('Recent Secrets') }}
-                                <span class="text-xs inline-block py-1 px-1.5 ml-1 leading-none text-center whitespace-nowrap align-baseline font-bold bg-gray-200 text-gray-700 rounded">
+                                    <x-jet-dropdown-link href="{{ route('recent') }}">
+                                        {{ __('Recent Secrets') }}
+                                        <span class="text-xs inline-block py-1 px-1.5 ml-1 leading-none text-center whitespace-nowrap align-baseline font-bold bg-gray-200 text-gray-700 rounded">
                                     {{ auth()->user()->texts()->whereDate('expires_at', '>', now())->count() }}
                                 </span>
-                            </x-jet-dropdown-link>
+                                    </x-jet-dropdown-link>
 
-                            <x-jet-dropdown-link href="{{ route('profile.show') }}">
-                                {{ __('Account Settings') }}
-                            </x-jet-dropdown-link>
+                                    <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                                        {{ __('Account Settings') }}
+                                    </x-jet-dropdown-link>
 
-                            @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                                <x-jet-dropdown-link href="{{ route('api-tokens.index') }}">
-                                    {{ __('API Tokens') }}
-                                </x-jet-dropdown-link>
-                            @endif
+                                    @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                                        <x-jet-dropdown-link href="{{ route('api-tokens.index') }}">
+                                            {{ __('API Tokens') }}
+                                        </x-jet-dropdown-link>
+                                    @endif
 
-                            <div class="border-t border-gray-100"></div>
+                                    <div class="border-t border-gray-100"></div>
 
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
+                                    <!-- Authentication -->
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
 
-                                <x-jet-dropdown-link href="{{ route('logout') }}"
-                                         onclick="event.preventDefault();
+                                        <x-jet-dropdown-link href="{{ route('logout') }}"
+                                                             onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                    {{ __('Log Out') }}
-                                </x-jet-dropdown-link>
-                            </form>
-                        </x-slot>
-                    </x-jet-dropdown>
+                                            {{ __('Log Out') }}
+                                        </x-jet-dropdown-link>
+                                    </form>
+                                </x-slot>
+                            </x-jet-dropdown>
+                        </div>
+                    @else
+                        <div class="space-x-8 -my-px ml-10 flex">
+                            <x-jet-nav-link href="{{ route('register') }}">
+                                {{ __('Create an account') }}
+                            </x-jet-nav-link>
+                            <x-jet-nav-link href="{{ route('login') }}">
+                                {{ __('Log in') }}
+                            </x-jet-nav-link>
+                        </div>
+                    @endauth
                 </div>
-                @else
-                <div class="space-x-8 -my-px ml-10 flex">
-                    <x-jet-nav-link href="{{ route('register') }}">
-                        {{ __('Create an account') }}
-                    </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('login') }}">
-                        {{ __('Log in') }}
-                    </x-jet-nav-link>
-                </div>
-                @endauth
             </div>
         </div>
-    </div>
-</nav>
+    </nav>
+</div>
