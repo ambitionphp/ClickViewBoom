05c594488202eaad7cd9306d1a901d092c830d6a
Steven Sarkisian <ambitionphp@gmail.com>
2022-01-31 23:45:41 -0500

    Emergency flush

--- resources/views/livewire/sponsor.blade.php
+++ resources/views/livewire/sponsor.blade.php
@@ -232,7 +232,7 @@
                     <div class="flex pt-3">
                         <div class="grow">
                             <button type="button" wire:click="continueToPaymentDetails" class="w-full px-4 py-2 bg-gray-800 text-center border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition shadow-xl">
-                                Make payment
+                                Payment details
                             </button>
                         </div>
                         <div class="flex-none pl-2">
