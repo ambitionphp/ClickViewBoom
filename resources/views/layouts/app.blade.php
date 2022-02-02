df05ac5edf2c92bff1a7303f05ff55e8d5ac2fae
Steven Sarkisian <ambitionphp@gmail.com>
2022-02-01 22:53:22 -0500

    update stats

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,15 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         {!! SEO::generate() !!}
         @include('layouts.favicon')
+
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/icon-256x256.png" />
+        <link rel="apple-touch-icon" href="/icon-384x384.png" />
+        <link rel="apple-touch-icon" href="/icon-512x512.png" />
+        <meta name="apple-mobile-web-app-status-bar" content="#f3f4f6" />
+        <meta name="theme-color" content="#1f2937" />
+
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         @livewireStyles
