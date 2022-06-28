95eacc3083d57333abe9c5d35740e4f909f6bca6
Steven Sarkisian <ambitionphp@gmail.com>
2022-06-28 13:54:26 -0400

    update stats

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,7 +4,7 @@
 
             <h1 class="font-semibold text-xl mb-2">About {{ config('app.name') }}</h1>
 
-            <p class="text-sm mb-3">I am Steven Sarkisian (AmbitionPHP) and I created {{ config('app.name') }} to give back to the development community. There are a few apps that provide a similar service, some of which even being open-source however I wanted to share my take at sharing private text with perhaps newer technology.</p>
+            <p class="text-sm mb-3">My name is Steven Sarkisian (AmbitionPHP) and I created {{ config('app.name') }} to give back to the development community. There are a few apps that provide a similar service, some of which even being open-source however I wanted to share my take at sharing private text with perhaps newer technology.</p>
 
             <p class="text-sm mb-5">{{ config('app.name') }} was created using Laravel 8, Livewire & TailWindCSS. You can view, download and even contribute to the source code this app uses anytime <a href="https://github.com/ambitionphp/ClickViewBoom" target="_blank">on github</a>.</p>
 
