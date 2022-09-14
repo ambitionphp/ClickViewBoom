3718b8657e43431fef016cec053f382e4cee9258
Steven Sarkisian <ambitionphp@gmail.com>
2022-09-14 14:31:43 -0400

    passport

--- resources/views/vendor/passport/authorize.blade.php
+++ resources/views/vendor/passport/authorize.blade.php
@@ -1,93 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>{{ config('app.name') }} - Authorization</title>
-
-    <!-- Styles -->
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-
-    <style>
-        .passport-authorize .container {
-            margin-top: 30px;
-        }
-
-        .passport-authorize .scopes {
-            margin-top: 20px;
-        }
-
-        .passport-authorize .buttons {
-            margin-top: 25px;
-            text-align: center;
-        }
-
-        .passport-authorize .btn {
-            width: 125px;
-        }
-
-        .passport-authorize .btn-approve {
-            margin-right: 15px;
-        }
-
-        .passport-authorize form {
-            display: inline;
-        }
-    </style>
-</head>
-<body class="passport-authorize">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card card-default">
-                    <div class="card-header">
-                        Authorization Request
-                    </div>
-                    <div class="card-body">
-                        <!-- Introduction -->
-                        <p><strong>{{ $client->name }}</strong> is requesting permission to access your account.</p>
-
-                        <!-- Scope List -->
-                        @if (count($scopes) > 0)
-                            <div class="scopes">
-                                    <p><strong>This application will be able to:</strong></p>
-
-                                    <ul>
-                                        @foreach ($scopes as $scope)
-                                            <li>{{ $scope->description }}</li>
-                                        @endforeach
-                                    </ul>
-                            </div>
-                        @endif
-
-                        <div class="buttons">
-                            <!-- Authorize Button -->
-                            <form method="post" action="{{ route('passport.authorizations.approve') }}">
-                                @csrf
-
-                                <input type="hidden" name="state" value="{{ $request->state }}">
-                                <input type="hidden" name="client_id" value="{{ $client->id }}">
-                                <input type="hidden" name="auth_token" value="{{ $authToken }}">
-                                <button type="submit" class="px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">Authorize</button>
-                            </form>
-
-                            <!-- Cancel Button -->
-                            <form method="post" action="{{ route('passport.authorizations.deny') }}">
-                                @csrf
-                                @method('DELETE')
-
-                                <input type="hidden" name="state" value="{{ $request->state }}">
-                                <input type="hidden" name="client_id" value="{{ $client->id }}">
-                                <input type="hidden" name="auth_token" value="{{ $authToken }}">
-                                <button class="px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 active:bg-red-600 disabled:opacity-25 transition">Cancel</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+<x-guest-layout>
+    <x-jet-authentication-card>
+        <x-slot name="logo">
+            <div class="text-center">
+                <x-jet-authentication-card-logo />
+                <h3 class="font-semibold text-xl">
+                    Authorization Request
+                </h3>
             </div>
+        </x-slot>
+
+        <!-- Scope List -->
+        @if (count($scopes) > 0)
+            <div class="scopes">
+                <p><strong>This application will be able to:</strong></p>
+
+                <ul>
+                    @foreach ($scopes as $scope)
+                        <li>{{ $scope->description }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <div class="buttons">
+            <!-- Authorize Button -->
+            <form method="post" action="{{ route('passport.authorizations.approve') }}">
+                @csrf
+
+                <input type="hidden" name="state" value="{{ $request->state }}">
+                <input type="hidden" name="client_id" value="{{ $client->id }}">
+                <input type="hidden" name="auth_token" value="{{ $authToken }}">
+                <button type="submit" class="px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">Authorize</button>
+            </form>
+
+            <!-- Cancel Button -->
+            <form method="post" action="{{ route('passport.authorizations.deny') }}">
+                @csrf
+                @method('DELETE')
+
+                <input type="hidden" name="state" value="{{ $request->state }}">
+                <input type="hidden" name="client_id" value="{{ $client->id }}">
+                <input type="hidden" name="auth_token" value="{{ $authToken }}">
+                <button class="px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 active:bg-red-600 disabled:opacity-25 transition">Cancel</button>
+            </form>
         </div>
-    </div>
-</body>
-</html>
+    </x-jet-authentication-card>
+</x-guest-layout>
